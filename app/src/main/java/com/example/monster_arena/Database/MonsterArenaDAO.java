1e482a0bc3cb7d813c84d6fecea57d6fe68a5a5b
Khanh Nguyen <con.nguyen.86@gmail.com>
2024-07-30 12:27:01 -0700

    Created MonsterArena POJO, Database object and DAO

---
+++ app/src/main/java/com/example/monster_arena/Database/MonsterArenaDAO.java
@@ -0,0 +1,19 @@
+package com.example.monster_arena.Database;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import com.example.monster_arena.Database.entities.MonsterArena;
+
+import java.util.List;
+
+@Dao
+public interface MonsterArenaDAO {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(MonsterArena monsterArena);
+
+    @Query("Select * from " + MonsterArenaDatabase.MONSTER_ARENA_TABLE)
+    List<MonsterArena> getAllRecord();
+}
--- app/src/main/java/com/example/monster_arena/Database/entities/MonsterArena.java
+++ app/src/main/java/com/example/monster_arena/Database/entities/MonsterArena.java
@@ -3,10 +3,20 @@ package com.example.monster_arena.Database.entities;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-@Entity(tableName = "monsterArena")
+import com.example.monster_arena.Database.MonsterArenaDatabase;
+
+@Entity(tableName = MonsterArenaDatabase.MONSTER_ARENA_TABLE)
 public class MonsterArena {
     @PrimaryKey(autoGenerate = true)
     private int id;
 
     //generate constructor, setters and getters, and equals and hashcode
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 }
