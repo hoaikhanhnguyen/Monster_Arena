37974d9b5849bdee1ff6c333ddff1f634ae60499
Khanh Nguyen <con.nguyen.86@gmail.com>
2024-07-31 20:25:21 -0700

    Added login page. Page Appears first with login UI. Methods not yet implemented. Changed theme

---
+++ app/src/main/java/com/example/monster_arena/LoginActivity.java
@@ -0,0 +1,35 @@
+package com.example.monster_arena;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.monster_arena.databinding.ActivityLoginBinding;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+
+        EdgeToEdge.enable(this);
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    static Intent loginIntentFactory(Context context) {
+        return new Intent(context, LoginActivity.class);
+    }
+}
--- app/src/main/java/com/example/monster_arena/MainActivity.java
+++ app/src/main/java/com/example/monster_arena/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.monster_arena;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,6 +10,9 @@ import com.example.monster_arena.database.MonsterArenaRepository;
 import com.example.monster_arena.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String MAIN_ACTIVITY_USER_ID = "com.example.monster_arena.MAIN_ACTIVITY_USER_ID";
+    int loggedInUserId = -1;
 
     ActivityMainBinding binding;
 
@@ -19,6 +24,24 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(binding.getRoot());
 
+        loginUser();
+
+        if(loggedInUserId == -1) {
+            Intent intent = LoginActivity.loginIntentFactory(getApplicationContext());
+            startActivity(intent);
+        }
+
         MonsterArenaRepository repository = MonsterArenaRepository.getRepository(getApplication());
+    }
+
+    private void loginUser() {
+        //TODO: create login method
+        loggedInUserId = getIntent().getIntExtra(MAIN_ACTIVITY_USER_ID, -1);
+    }
+
+    static Intent mainActivityIntentFactory(Context context, int userId) {
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra(MAIN_ACTIVITY_USER_ID, userId);
+        return intent;
     }
 }
