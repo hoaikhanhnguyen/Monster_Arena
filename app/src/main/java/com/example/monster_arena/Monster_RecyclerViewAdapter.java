0a10c51db601a01bdf5132e83bfadec4fbaf1520
rsergeg <101226563+rsergeg@users.noreply.github.com>
2024-08-06 18:29:38 -0700

    cleaned up spacing in all my files, cleaned up unused imports.

--- app/src/main/java/com/example/monster_arena/Monster_RecyclerViewAdapter.java
+++ app/src/main/java/com/example/monster_arena/Monster_RecyclerViewAdapter.java
@@ -1,7 +1,6 @@
 package com.example.monster_arena;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
