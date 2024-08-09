313675e8b12a4bea7e66f880f0bb19eb400e3bb2
dracela <doracela@csumb.edu>
2024-08-08 22:21:30 -0700

    updated BattleDaoTest

--- app/src/androidTest/java/com/example/monster_arena/database/BattleDaoTest.java
+++ app/src/androidTest/java/com/example/monster_arena/database/BattleDaoTest.java
@@ -1,6 +1,9 @@
 package com.example.monster_arena.database;
 
 
+import static junit.framework.TestCase.assertEquals;
+
+import androidx.lifecycle.LiveData;
 import androidx.room.Room;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
@@ -12,6 +15,8 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.List;
 
 @RunWith(AndroidJUnit4.class)
 @SmallTest
@@ -39,15 +44,35 @@ public class BattleDaoTest {
         Battle battle = new Battle("monster", "enemy", 0, "user", "arena");
         dao.insert(battle);
 
-        Battle retrievedBattle = dao.getBattles()
-    }
-
-    public void deleteBattleTest() {
-
+        List<Battle> allBattles = dao.getBattles();
+        Battle retrievedBattle = allBattles.get(0);
+        assertEquals("monster", retrievedBattle.getMonster_name());
+        assertEquals("enemy", retrievedBattle.getOpponent_name());
+        assertEquals(0, retrievedBattle.getUser_id());
+        assertEquals("user", retrievedBattle.getUser_name());
+        assertEquals("arena", retrievedBattle.getArena_name());
     }
 
     @Test
-    public void updateBattleTest() {
+    public void deleteAllBattleTest() {
+        Battle battle = new Battle("monster", "enemy", 0, "user", "arena");
+        Battle battle1 = new Battle("monster1", "enemy1", 1, "user1", "arena1");
+        dao.insert(battle, battle1);
 
+        List<Battle> allBattles = dao.getBattles();
+        assertEquals(2, allBattles.size());
+
+        dao.deleteAll();
+        allBattles = dao.getBattles();
+        assertEquals(0,allBattles.size());
+    }
+
+    @Test
+    public void getRecentBattleTest() {
+        Battle battle = new Battle("monster", "enemy", 0, "user", "arena");
+        Battle battle1 = new Battle("monster1", "enemy1", 1, "user1", "arena1");
+        dao.insert(battle, battle1);
+
+        LiveData<Battle> liveData = dao.getRecentBattle();
     }
 }
